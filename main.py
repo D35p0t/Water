95b1176b2e50f8e27ffa3db8280b48c48751ad6f
D35p0t <vladvpolevoin@gmail.com>
2023-11-26 16:36:33 +0300

    Begin of commit history. it has already a lot of done: standart matrix, method runge-kutta and algorithm of solving

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.animation as animation
-
+from scipy.sparse import spdiags
 
 def diff4_segment(f):
     diff_f = np.zeros([2, N + 1])
@@ -82,20 +82,15 @@ def matrix_factory():
         G[i, i - 2:i + 3] = [1 / 12, -2 / 3, 0, 2 / 3, -1 / 12]
     return np.linalg.inv(H) @ G, H
 
-def upg_matrix_factory(order):
+def upg_matrix_factory():
     BP = 4
     NES = 2
-    xb = np.zeros(5)
-    xb[0] = 0.0000000000000e+00
-    xb[1] = 6.8764546205559e-01
-    xb[2] = 1.8022115125776e+00
-    xb[3] = 2.8022115125776e+00
-    xb[4] = 3.8022115125776e+00
+    xb = np.array([0, 0.68764546205559, 1.8022115125776, 2.8022115125776, 3.8022115125776])
     for i in range(5 - (NES+1)):
         xb = xb[:-1]
     global dx, x
     dx = (b - a)/(2 * xb[-1] + N - 1 - 2*NES)
-    x = dx * np.concatenate([xb, np.linspace(xb[-1]+1, (b - a)/dx - xb[-1]-1, N-2*(NES+1)).transpose(), (b - a)/dx - np.flip(xb, 0)])
+    x = a + dx * np.concatenate([xb, np.linspace(xb[-1]+1, (b - a)/dx - xb[-1]-1, N+1-2*(NES+1)).transpose(), (b - a)/dx - np.flip(xb, 0)])
     P = np.zeros(BP)
     P[0] = 2.1259737557798e-01
     P[1] = 1.0260290400758e+00
@@ -105,14 +100,23 @@ def upg_matrix_factory(order):
         P = P[:-1]
     A = np.ones(N+1)
     A[0:BP] = P
-    A[N-BP:-1] = np.flip(P, 0)
+    A[N-BP+1:N+1] = np.flip(P, 0)
     H = np.zeros([N+1, N+1])
-    np.fill_diagonal(H, A)
-    Q = np.zeros([N+1, N+1])
-    for i in range(4, N - 3):
-        Q[i, i - 2:i + 3] = [1 / 12, -2 / 3, 0, 2 / 3, -1 / 12]
-    
-    print(Q)
+    np.fill_diagonal(H, dx * A)
+    d = [[1 / 12], [-2/3], [0], [2/3], [1/12]]
+    G = np.zeros([N + 1, N + 1])
+    for i in range(5):
+        G += (spdiags(d[i]*(N+1), i-2, N+1, N+1).toarray())
+    Bound = np.array([[-0.5, 6.5605279837843e-01, -1.9875859409017e-01, 4.2705795711740e-02, 0, 0],
+             [-6.5605279837843e-01, 0, 8.1236966439895e-01, -1.5631686602052e-01, 0, 0],
+             [1.9875859409017e-01, -8.1236966439895e-01, 0, 6.9694440364211e-01, -1/12, 0],
+             [-4.2705795711740e-02,  1.5631686602052e-01,  -6.9694440364211e-01, 0, 2/3, -1/12]])
+    for i in range(BP):
+        for j in range(BP):
+            G[i, j] = Bound[i, j]
+            G[N-i, N-j] = -Bound[i, j]
+    return np.linalg.inv(H) @ G, H, x, dx
+
 
 def Energy():
     A = np.eye(N+1, N+1)
@@ -133,15 +137,15 @@ a = -15
 b = 15
 N = 150
 Nt = 1000
-dx = (b - a) / N
-D, H = matrix_factory()
-x = a + dx * np.arange(0, N + 1)
-dt = dx / (np.sqrt(g * h0) * 4)
-ini_state = np.array([np.zeros(len(x)), np.exp((-0.5) * (x ** 2)) / np.sqrt(2 * np.pi)])
+#dx = (b - a) / N
+#D, H = matrix_factory()
+#x = a + dx * np.arange(0, N + 1)
 #ini_state = np.array([np.exp((-0.5) * (x ** 2)) / np.sqrt(2 * np.pi), np.exp((-0.5) * (x ** 2)) / np.sqrt(2 * np.pi)])
 #ini_state = np.array([np.zeros(len(x)), np.zeros(len(x))])
-#upg_matrix_factory(4)
+D, H, x, dx = upg_matrix_factory()
+dt = dx / (np.sqrt(g * h0) * 4)
+ini_state = np.array([np.zeros(x.size), np.exp((-0.5) * (x ** 2)) / np.sqrt(2 * np.pi)])
 solution = solveEquation(RK4D)
-#Energy()
-ani = animatewater(solution)
+Energy()
+#ani = animatewater(solution)
 #ani.save('wall_water.gif', fps=25)
