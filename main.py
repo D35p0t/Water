2ff8f589da6087ceeea1ea019526309b9f22c14c
D35p0t <vladvpolevoin@gmail.com>
2023-12-01 22:15:53 +0300

    Сделан класс для нового метода

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from scipy.sparse import spdiags
-import math
 
 
 class solve_typeI:
@@ -11,19 +10,19 @@ class solve_typeI:
         self.N = N
         self.Nt = Nt
         self.t_ac = t_ac
-        self.dt = None
         self.type_in = type_in
+        self.dt = None
         self.x = None
         self.dx = None
         self.D = None
         self.sol = None
         self.H = None
 
-    def RK4D(self, state):
-        k1 = solve_typeI.matrix_diff(self, state)
-        k2 = solve_typeI.matrix_diff(self, state + k1 * self.dt / 2)
-        k3 = solve_typeI.matrix_diff(self, state + k2 * self.dt / 2)
-        k4 = solve_typeI.matrix_diff(self, state + k3 * self.dt)
+    def RK4D(self, state, diff_method):
+        k1 = diff_method(state)
+        k2 = diff_method(state + k1 * self.dt / 2)
+        k3 = diff_method(state + k2 * self.dt / 2)
+        k4 = diff_method(state + k3 * self.dt)
         return state + self.dt * (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
     def matrix_diff(self, f):
@@ -67,14 +66,52 @@ class solve_typeI:
 
     def solveEquation(self, method):
         global g, h0
-        solve_typeI.matrix_factory(self)
-        self.dt = self.dx / (np.sqrt(g * h0) * 4)
+        self.matrix_factory()
+        self.dt = self.dx / (np.sqrt(g * h0) * self.t_ac)
         sol = np.zeros([self.Nt + 1, 2, self.N + 1])
         sol[0, :, :] = solve_typeI.in_state(self)
         for i in range(1, self.Nt + 1):
-            sol[i, :, :] = method(sol[i - 1, :, :])
+            sol[i, :, :] = method(sol[i - 1, :, :], self.matrix_diff)
         self.sol = sol
 
+
+class solve_typeII(solve_typeI):
+    def matrix_factory(self):
+        BP = 4
+        NES = 2
+        xb = np.array([0, 0.68764546205559, 1.8022115125776, 2.8022115125776, 3.8022115125776])
+        for i in range(5 - (NES + 1)):
+            xb = xb[:-1]
+        self.dx = (self.sp[1] - self.sp[0]) / (2 * xb[-1] + self.N - 2 * NES)
+        self.x = self.sp[0] + self.dx * np.concatenate(
+            [xb, np.linspace(xb[-1] + 1, (self.sp[1] - self.sp[0]) / self.dx - xb[-1] - 1, self.N + 1 - 2 * (NES + 1)).transpose(),
+             (self.sp[1] - self.sp[0]) / self.dx - np.flip(xb, 0)])
+        P = np.zeros(BP)
+        P[0] = 2.1259737557798e-01
+        P[1] = 1.0260290400758e+00
+        P[2] = 1.0775123588954e+00
+        P[3] = 9.8607273802835e-01
+        for i in range(4 - BP):
+            P = P[:-1]
+        A = np.ones(self.N + 1)
+        A[0:BP] = P
+        A[self.N - BP + 1:self.N + 1] = np.flip(P, 0)
+        H = np.zeros([self.N + 1, self.N + 1])
+        np.fill_diagonal(H, self.dx * A)
+        d = [[1 / 12], [-2 / 3], [0], [2 / 3], [-1 / 12]]
+        G = np.zeros([self.N + 1, self.N + 1])
+        for i in range(5):
+            G += (spdiags(d[i] * (self.N + 1), i - 2, self.N + 1, self.N + 1).toarray())
+        Bound = np.array([[-0.5, 6.5605279837843e-01, -1.9875859409017e-01, 4.2705795711740e-02, 0, 0],
+                          [-6.5605279837843e-01, 0, 8.1236966439895e-01, -1.5631686602052e-01, 0, 0],
+                          [1.9875859409017e-01, -8.1236966439895e-01, 0, 6.9694440364211e-01, -1 / 12, 0],
+                          [-4.2705795711740e-02, 1.5631686602052e-01, -6.9694440364211e-01, 0, 2 / 3, -1 / 12]])
+        for i in range(BP):
+            for j in range(BP):
+                G[i, j] = Bound[i, j]
+                G[self.N - i, self.N - j] = -Bound[i, j]
+        self.D = np.linalg.lstsq(H, G, rcond=None)[0]
+        self.H = H
 
 def animate_water(sol):
     fig = plt.figure()
@@ -99,17 +136,13 @@ def animate_water(sol):
 
 g = 9.81
 h0 = 10
-solution = solve_typeI([-15, 15], 150, 1000, 4, 0)
+solution = solve_typeII([-15, 15], 150, 1000, 4, 1)
 solution.solveEquation(solution.RK4D)
 ani = animate_water(solution)
-
 #ani.save('water.gif', fps=25)
+
 """
-
-solveEquation(RK4D, matrix_factory, 0)
-
-
-
+OUTDATED METHODS
 
 def diff4_segment(f):
     diff_f = np.zeros([2, N + 1])
@@ -121,74 +154,4 @@ def diff4_segment(f):
                 f[0, (i - 2 + N) % N] / 12 - (2 / 3) * f[0, (i - 1 + N) % N] + (2 / 3) * f[0, (i + 1 + N) % N] -
                 f[0, (i + 2 + N) % N] / 12) / dx
     return diff_f
-
-def matrix_diff(f):
-    nonlocal D, dx
-    diff = (D @ f.transpose() / dx).transpose()
-    diff = np.flip(diff, 0)
-    diff[0, :] = -g * diff[0, :]
-    diff[1, :] = -h0 * diff[1, :]
-    diff[0, 0] = 0
-    diff[0, -1] = 0
-    return diff
-
-
-def RK4D(state):
-    nonlocal dt
-    k1 = matrix_diff(state)
-    k2 = matrix_diff(state + k1 * dt / 2)
-    k3 = matrix_diff(state + k2 * dt / 2)
-    k4 = matrix_diff(state + k3 * dt)
-    return state + dt * (k1 + 2 * k2 + 2 * k3 + k4) / 6
-
-
-def solveEquation(method, matrix_type, m):
-    D, H, x, dx = matrix_type()
-    dt = dx / (np.sqrt(g * h0) * 4)
-    sol = np.zeros([Nt + 1, 2, N + 1])
-    sol[0, :, :] = in_type(m, x)
-    for i in range(1, Nt + 1):
-        sol[i, :, :] = method(sol[i - 1, :, :])
-    return sol
-
-
-
-def upg_matrix_factory():
-    BP = 4
-    NES = 2
-    xb = np.array([0, 0.68764546205559, 1.8022115125776, 2.8022115125776, 3.8022115125776])
-    for i in range(5 - (NES + 1)):
-        xb = xb[:-1]
-    dx = (b - a) / (2 * xb[-1] + N - 2 * NES)
-    x = a + dx * np.concatenate(
-        [xb, np.linspace(xb[-1] + 1, (b - a) / dx - xb[-1] - 1, N + 1 - 2 * (NES + 1)).transpose(),
-         (b - a) / dx - np.flip(xb, 0)])
-    P = np.zeros(BP)
-    P[0] = 2.1259737557798e-01
-    P[1] = 1.0260290400758e+00
-    P[2] = 1.0775123588954e+00
-    P[3] = 9.8607273802835e-01
-    for i in range(4 - BP):
-        P = P[:-1]
-    A = np.ones(N + 1)
-    A[0:BP] = P
-    A[N - BP + 1:N + 1] = np.flip(P, 0)
-    H = np.zeros([N + 1, N + 1])
-    np.fill_diagonal(H, dx * A)
-    d = [[1 / 12], [-2 / 3], [0], [2 / 3], [-1 / 12]]
-    G = np.zeros([N + 1, N + 1])
-    for i in range(5):
-        G += (spdiags(d[i] * (N + 1), i - 2, N + 1, N + 1).toarray())
-    Bound = np.array([[-0.5, 6.5605279837843e-01, -1.9875859409017e-01, 4.2705795711740e-02, 0, 0],
-                      [-6.5605279837843e-01, 0, 8.1236966439895e-01, -1.5631686602052e-01, 0, 0],
-                      [1.9875859409017e-01, -8.1236966439895e-01, 0, 6.9694440364211e-01, -1 / 12, 0],
-                      [-4.2705795711740e-02, 1.5631686602052e-01, -6.9694440364211e-01, 0, 2 / 3, -1 / 12]])
-    for i in range(BP):
-        for j in range(BP):
-            G[i, j] = Bound[i, j]
-            G[N - i, N - j] = -Bound[i, j]
-    return np.linalg.lstsq(H, G, rcond=None)[0], H, x, dx
-
-
-def compare(method1, method2):
-    print(1)"""
+"""
